a3f993866e8507fa9c014bf66c7c16912c675ed2
vnikolaou <vnikolaou12@gmail.com>
2018-05-04 03:29:39 +0300

    Fixing UI issues

--- src/main/java/org/gluu/oidc/rpserver/controller/BaseController.java
+++ src/main/java/org/gluu/oidc/rpserver/controller/BaseController.java
@@ -1,14 +1,36 @@
 package org.gluu.oidc.rpserver.controller;
 
+import org.gluu.oidc.rpserver.domain.Info;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public abstract class BaseController {
 	@Value("${app.version}")
 	private String appVersion;
-	
-	@ModelAttribute("appVersion")
-	public String getAppVersion() {
-		return appVersion;
+	@Value("${app.email}")	
+	private String email;
+	@Value("${app.copyright}")	
+	private String copyright;
+	@Value("${app.org.name}")	
+	private String orgName;
+	@Value("${app.org.url}")	
+	private String orgUrl;
+	@Value("${app.issues.host.name}")	
+	private String issuesHostName;
+	@Value("${app.issues.host.url}")	
+	private String issuesHostUrl;
+				
+	@ModelAttribute("info")
+	public Info getAppVersion() {
+		Info appInfo = new Info();
+		appInfo.setAppVersion(appVersion);
+		appInfo.setCopyright(copyright);
+		appInfo.setEmail(email);
+		appInfo.setIssuesHostName(issuesHostName);
+		appInfo.setIssuesHostUrl(issuesHostUrl);
+		appInfo.setOrgName(orgName);
+		appInfo.setOrgUrl(orgUrl);
+		
+		return appInfo;
 	}
 }
---
+++ src/main/java/org/gluu/oidc/rpserver/domain/Info.java
@@ -0,0 +1,65 @@
+package org.gluu.oidc.rpserver.domain;
+
+public class Info {
+	private String appVersion;
+	private String email;
+	private String copyright;
+	private String orgName;
+	private String orgUrl;
+	private String issuesHostName;
+	private String issuesHostUrl;
+	
+	public Info() {
+	}
+	
+	public String getAppVersion() {
+		return appVersion;
+	}
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCopyright() {
+		return copyright;
+	}
+	public void setCopyright(String copyright) {
+		this.copyright = copyright;
+	}
+	public String getOrgName() {
+		return orgName;
+	}
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+	public String getOrgUrl() {
+		return orgUrl;
+	}
+	public void setOrgUrl(String orgUrl) {
+		this.orgUrl = orgUrl;
+	}
+	public String getIssuesHostName() {
+		return issuesHostName;
+	}
+	public void setIssuesHostName(String issuesHostName) {
+		this.issuesHostName = issuesHostName;
+	}
+	public String getIssuesHostUrl() {
+		return issuesHostUrl;
+	}
+	public void setIssuesHostUrl(String issuesHostUrl) {
+		this.issuesHostUrl = issuesHostUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "Info [appVersion=" + appVersion + ", email=" + email + ", copyright=" + copyright + ", orgName="
+				+ orgName + ", orgUrl=" + orgUrl + ", issuesHostName=" + issuesHostName + ", issuesHostUrl="
+				+ issuesHostUrl + "]";
+	}
+	
+}
